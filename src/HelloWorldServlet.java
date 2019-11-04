9ab763ab43df7e8bdb1689c9c9479c16743fe309
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2019-11-04 09:30:15 -0600

    add hello servlet

--- src/HelloWorldServlet.java
+++ src/HelloWorldServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
